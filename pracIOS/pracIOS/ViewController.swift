ac87f5967c229a19b402e3759232d9eb4880e2ea
downpool <wanpile@g.cnu.ac.kr>
2021-12-22 10:08:31 +0900

    base

---
+++ pracIOS/pracIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pracIOS
+//
+//  Created by 최동현 on 2021/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pracIOS/pracIOSUITests/pracIOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pracIOSUITestsLaunchTests.swift
+//  pracIOSUITests
+//
+//  Created by 최동현 on 2021/12/21.
+//
+
+import XCTest
+
+class pracIOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
