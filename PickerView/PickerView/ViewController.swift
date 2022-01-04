77861432af22e69e2e8b4dd49924f9e36962a47e
donghyeon choi <wanpile@g.cnu.ac.kr>
2022-01-04 17:31:36 +0900

    chap5

--- PickerView/PickerView/ViewController.swift
+++ PickerView/PickerView/ViewController.swift
@@ -11,13 +11,23 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     @IBOutlet var pickerImage: UIPickerView!
     @IBOutlet var lblImageFileName: UILabel!
     @IBOutlet var imageView: UIImageView!
+    
     let MAX_ARRAY_NUM = 10
     let PICKER_VIEW_COLUMN = 1
+    var imageArray = [UIImage?]()
     var imageFileName = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        for i in 0 ..< MAX_ARRAY_NUM{
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        lblImageFileName.text = imageFileName[0]
+        imageView.image = imageArray[0]
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -34,6 +44,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         lblImageFileName.text = imageFileName[row]
+        imageView.image = imageArray[row]
     }
 }
 // var imageFileName = ["./image/1.jpg", "./image/2.jpg", "./image/3.jpg", "./image/4.jpg", "./image/5.jpg", "./image/6.jpg", "./image/7.jpg", "./image/8.jpg", "./image/9.jpg", "./image/10.jpg"]
