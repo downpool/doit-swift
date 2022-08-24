db72b4f5d8ac998ebb56b31a862f3b0d178db90b
donghyeon choi <wanpile@g.cnu.ac.kr>
2022-08-24 16:32:13 +0900

    objective
    2pomo

--- CodeCademy.swiftpm/SwiftBank.playground/Contents.swift
+++ CodeCademy.swiftpm/SwiftBank.playground/Contents.swift
@@ -1,3 +1,22 @@
-import UIKit
 
-var greeting = "Hello, playground"
+
+struct SwiftBank {
+    private let password: String
+    
+    var balance = 0
+    
+    
+    init(password: String) {
+        self.password = password
+    }
+    
+    private func isValid(enteredPassword: String) -> Bool {
+        if enteredPassword == password {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+print(type(of: sb.balance))
